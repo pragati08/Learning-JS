cf3635ac385e132b8f3155a660bf91f8c18a302f
pragati08 <yadao.pragati08@gmail.com>
2025-04-22 11:55:45 +0530

    crypto setup

---
+++ index.js
@@ -0,0 +1,14 @@
+// API request options
+
+const options = {
+  method: "GET",
+  header: {
+    accept: "application/json",
+    "x-cg-demo-api-key": "CG-mDVVqLm5jvcVq523LnAmB",
+  },
+};
+
+let coins = [];
+let currentPage = 1;
+
+const fetchCoins = async();
