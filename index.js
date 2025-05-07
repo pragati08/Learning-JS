48bedc5aa60aa9ca99f264a482653e7bae6ea12a
pragati08 <yadao.pragati08@gmail.com>
2025-05-07 12:46:56 +0530

    post request using postman

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.method == "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      console.log(body);
+      res.end("Data received !!");
+    });
+  } else {
+    res.end("Welcome to node JS !!");
+  }
+});
+
+server.listen(3100);
